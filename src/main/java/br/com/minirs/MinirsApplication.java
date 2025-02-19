34abc9176cb8aedcc96529ea302e21fc8f15918b
Gabriel <144123550+brielpg@users.noreply.github.com>
2025-02-19 15:41:18 -0300

    feat: Inicio do projeto

---
+++ src/main/java/br/com/minirs/MinirsApplication.java
@@ -0,0 +1,13 @@
+package br.com.minirs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinirsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinirsApplication.class, args);
+	}
+
+}
