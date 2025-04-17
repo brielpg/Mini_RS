6f07e7aaea719c91e5a51d6deafe48c7b7e6e283
Gabriel <144123550+brielpg@users.noreply.github.com>
2025-04-17 00:55:00 -0300

    feat: update GlobalExceptionHandler to support new custom exceptions

--- src/main/java/br/com/minirs/infra/GlobalExceptionHandler.java
+++ src/main/java/br/com/minirs/infra/GlobalExceptionHandler.java
@@ -1,6 +1,10 @@
 package br.com.minirs.infra;
 
 import br.com.minirs.dto.ExceptionDto;
+import br.com.minirs.exceptions.post.LikedPostsException;
+import br.com.minirs.exceptions.post.PostDeletedException;
+import br.com.minirs.exceptions.post.PostNotFoundException;
+import br.com.minirs.exceptions.post.UnauthorizedUserException;
 import br.com.minirs.exceptions.user.EmailAlreadyRegisteredException;
 import br.com.minirs.exceptions.user.UserDisabledException;
 import br.com.minirs.exceptions.user.UserNameAlreadyRegisteredException;
@@ -13,6 +17,28 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(UnauthorizedUserException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public ResponseEntity<ExceptionDto> handleUnauthorizedUserException(UnauthorizedUserException ex) {
+        var dto = new ExceptionDto(HttpStatus.UNAUTHORIZED.value(), HttpStatus.UNAUTHORIZED, ex.getMessage());
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(dto);
+    }
+
+    @ExceptionHandler(PostDeletedException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ResponseEntity<ExceptionDto> handlePostDeletedException(PostDeletedException ex) {
+        var dto = new ExceptionDto(HttpStatus.CONFLICT.value(), HttpStatus.CONFLICT, ex.getMessage());
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(dto);
+    }
+
+    @ExceptionHandler(LikedPostsException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ResponseEntity<ExceptionDto> handleLikedPostsException(LikedPostsException ex) {
+        var dto = new ExceptionDto(HttpStatus.CONFLICT.value(), HttpStatus.CONFLICT, ex.getMessage());
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(dto);
+    }
+
     @ExceptionHandler(EmailAlreadyRegisteredException.class)
     @ResponseStatus(HttpStatus.CONFLICT)
     public ResponseEntity<ExceptionDto> handleEmailAlreadyRegisteredException(EmailAlreadyRegisteredException ex) {
@@ -29,6 +55,13 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<?> handleUserNotFoundException(UserNotFoundException ex) {
+        var dto = new ExceptionDto(HttpStatus.NOT_FOUND.value(), HttpStatus.NOT_FOUND, ex.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(dto);
+    }
+
+    @ExceptionHandler(PostNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<ExceptionDto> handlePostNotFoundException(PostNotFoundException ex) {
         var dto = new ExceptionDto(HttpStatus.NOT_FOUND.value(), HttpStatus.NOT_FOUND, ex.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(dto);
     }
