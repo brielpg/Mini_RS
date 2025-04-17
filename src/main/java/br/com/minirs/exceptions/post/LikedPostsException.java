1cf6883944b5e813f032310bbc36c82a6c5514a4
Gabriel <144123550+brielpg@users.noreply.github.com>
2025-04-17 00:50:17 -0300

    feat: add LikedPostsException exception

---
+++ src/main/java/br/com/minirs/exceptions/post/LikedPostsException.java
@@ -0,0 +1,7 @@
+package br.com.minirs.exceptions.post;
+
+public class LikedPostsException extends RuntimeException{
+    public LikedPostsException(String message){
+        super(message);
+    }
+}
