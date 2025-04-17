2fa879181acec3cfdea4fbeb0a1f99794f05bb6c
Gabriel <144123550+brielpg@users.noreply.github.com>
2025-04-16 21:52:28 -0300

    feat: update GlobalExceptionHandler to support new custom exceptions

--- src/main/java/br/com/minirs/infra/GlobalExceptionHandler.java
+++ src/main/java/br/com/minirs/infra/GlobalExceptionHandler.java
@@ -1,6 +1,10 @@
 package br.com.minirs.infra;
 
 import br.com.minirs.dto.ExceptionDto;
+import br.com.minirs.exceptions.user.EmailAlreadyRegisteredException;
+import br.com.minirs.exceptions.user.UserDisabledException;
+import br.com.minirs.exceptions.user.UserNameAlreadyRegisteredException;
+import br.com.minirs.exceptions.user.UserNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -9,6 +13,32 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(EmailAlreadyRegisteredException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ResponseEntity<ExceptionDto> handleEmailAlreadyRegisteredException(EmailAlreadyRegisteredException ex) {
+        var dto = new ExceptionDto(HttpStatus.CONFLICT.value(), HttpStatus.CONFLICT, ex.getMessage());
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(dto);
+    }
+
+    @ExceptionHandler(UserNameAlreadyRegisteredException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ResponseEntity<ExceptionDto> handleUserNameAlreadyRegisteredException(UserNameAlreadyRegisteredException ex) {
+        var dto = new ExceptionDto(HttpStatus.CONFLICT.value(), HttpStatus.CONFLICT, ex.getMessage());
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(dto);
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<?> handleUserNotFoundException(UserNotFoundException ex) {
+        var dto = new ExceptionDto(HttpStatus.NOT_FOUND.value(), HttpStatus.NOT_FOUND, ex.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(dto);
+    }
+
+    @ExceptionHandler(UserDisabledException.class)
+    public ResponseEntity<?> handleUserDisabledException(UserDisabledException ex) {
+        var dto = new ExceptionDto(HttpStatus.FORBIDDEN.value(), HttpStatus.FORBIDDEN, ex.getMessage());
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(dto);
+    }
+
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ResponseEntity<ExceptionDto> handleGenericException(Exception ex) {
