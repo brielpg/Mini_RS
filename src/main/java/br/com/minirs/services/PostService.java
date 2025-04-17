431c329ccfa2d2f8d03bf63d3dd94d344d3ae399
Gabriel <144123550+brielpg@users.noreply.github.com>
2025-04-17 00:56:34 -0300

    refactor: update service to use validation methods and integrate exceptions
    - Replaced direct validation logic with dedicated validation methods.

--- src/main/java/br/com/minirs/services/PostService.java
+++ src/main/java/br/com/minirs/services/PostService.java
@@ -5,6 +5,11 @@ import br.com.minirs.dto.post.DtoReturnPost;
 import br.com.minirs.dto.post.DtoUpdatePost;
 import br.com.minirs.dto.reactions.DtoLike;
 import br.com.minirs.entities.Post;
+import br.com.minirs.entities.User;
+import br.com.minirs.exceptions.post.LikedPostsException;
+import br.com.minirs.exceptions.post.PostDeletedException;
+import br.com.minirs.exceptions.post.PostNotFoundException;
+import br.com.minirs.exceptions.post.UnauthorizedUserException;
 import br.com.minirs.repositories.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -26,12 +31,10 @@ public class PostService {
 
     @Transactional
     public ResponseEntity<?> createPost(DtoCreatePost data) {
-        if (!userService.existsById(data.userId())){
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("ERROR: User not found.");
-        }
+        userService.validateUserExistsById(data.userId());
 
         var user = userService.getReferenceById(data.userId());
-        user.setPostCount(user.getPostCount()+1);
+        user.setPostCount(user.getPostCount() + 1);
         var newPost = new Post(data, user);
 
         this.save(newPost);
@@ -41,137 +44,119 @@ public class PostService {
 
     @Transactional
     public ResponseEntity<?> deletePost(Long postId, Long userId) {
-        if (this.existsById(postId)){
-            var post = this.getReferenceById(postId);
-            var user = userService.getReferenceById(userId);
+        validatePostExistsById(postId);
 
-            if (post.getPostOwner() != user) return ResponseEntity.status(HttpStatus.CONFLICT).body("ERROR: User is not allowed to delete this post.");
+        var post = this.getReferenceById(postId);
+        var user = userService.getReferenceById(userId);
 
-            if (post.getActive()){
-                post.setActive(false);
-                user.setPostCount(user.getPostCount()-1);
+        validatePostOwner(post,user,"User is not allowed to delete this post");
 
-                userService.save(user);
-                this.save(post);
+        validatePostActive(post);
 
-                return ResponseEntity.status(HttpStatus.OK).body(new DtoReturnPost(post));
-            }
-            return ResponseEntity.status(HttpStatus.CONFLICT).body("ERROR: Post is already deleted.");
-        }
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("ERROR: Post not found.");
+        post.setActive(false);
+        user.setPostCount(user.getPostCount() - 1);
+
+        userService.save(user);
+        this.save(post);
+
+        return ResponseEntity.status(HttpStatus.OK).body(new DtoReturnPost(post));
     }
 
     @Transactional
     public ResponseEntity<?> reactivatePost(Long postId, Long userId) {
-        if (this.existsById(postId)){
-            var post = this.getReferenceById(postId);
-            var user = userService.getReferenceById(userId);
+        validatePostExistsById(postId);
 
-            if (post.getPostOwner() != user) return ResponseEntity.status(HttpStatus.CONFLICT).body("ERROR: User is not allowed to reactivate this post.");
+        var post = this.getReferenceById(postId);
+        var user = userService.getReferenceById(userId);
 
-            if (!post.getActive()){
-                post.setActive(true);
-                user.setPostCount(user.getPostCount()+1);
+        validatePostOwner(post, user, "User is not allowed to reactivate this post");
 
-                userService.save(user);
-                this.save(post);
+        if (post.getActive()) throw new PostDeletedException("Post is already active");
 
-                return ResponseEntity.status(HttpStatus.OK).body(new DtoReturnPost(post));
-            }
-            return ResponseEntity.status(HttpStatus.CONFLICT).body("ERROR: Post is already active.");
-        }
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("ERROR: Post not found.");
+        post.setActive(true);
+        user.setPostCount(user.getPostCount() + 1);
+
+        userService.save(user);
+        this.save(post);
+
+        return ResponseEntity.status(HttpStatus.OK).body(new DtoReturnPost(post));
     }
 
     @Transactional
     public ResponseEntity<?> updatePost(DtoUpdatePost data) {
-        if (this.existsById(data.postId())){
-            var post = this.getReferenceById(data.postId());
-            var user = userService.getReferenceById(data.userId());
+        validatePostExistsById(data.postId());
 
-            if (post.getPostOwner() != user) return ResponseEntity.status(HttpStatus.CONFLICT).body("ERROR: User is not allowed to update this post.");
+        var post = this.getReferenceById(data.postId());
+        var user = userService.getReferenceById(data.userId());
 
-            if (post.getActive()){
-                post.updatePost(data.content());
-                this.save(post);
+        validatePostOwner(post, user, "User is not allowed to update this post");
 
-                return ResponseEntity.status(HttpStatus.OK).body(new DtoReturnPost(post));
-            }
-            return ResponseEntity.status(HttpStatus.CONFLICT).body("ERROR: Post is deleted.");
-        }
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("ERROR: Post not found.");
+        validatePostActive(post);
+
+        post.updatePost(data.content());
+        this.save(post);
+
+        return ResponseEntity.status(HttpStatus.OK).body(new DtoReturnPost(post));
     }
 
     @Transactional(readOnly = true)
     public ResponseEntity<?> getPostsByUser(Long userId) {
-        if (userService.existsById(userId)){
-            var user = userService.getReferenceById(userId);
-            if (user.getActive()){
-                var postsByOwner = postRepository.findActivePostsByPostOwner(user);
-                List<DtoReturnPost> posts = postsByOwner.stream()
-                        .map(DtoReturnPost::new)
-                        .toList();
+        userService.validateUserExistsById(userId);
+        var user = userService.getReferenceById(userId);
+        userService.validateUserActive(user);
 
-                return ResponseEntity.status(HttpStatus.OK).body(posts);
-            }
-            return ResponseEntity.status(HttpStatus.CONFLICT).body("ERROR: User is disabled.");
-        }
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("ERROR: User not found.");
+        var postsByOwner = postRepository.findActivePostsByPostOwner(user);
+        List<DtoReturnPost> posts = postsByOwner.stream()
+                .map(DtoReturnPost::new)
+                .toList();
+
+        return ResponseEntity.status(HttpStatus.OK).body(posts);
     }
 
     @Transactional(readOnly = true)
     public ResponseEntity<?> getFollowingUsersPosts(Long userId) {
-        if (userService.existsById(userId)){
-            var user = userService.getReferenceById(userId);
-            if (user.getActive()){
-                var posts = user.getFollowing().stream()
-                        .flatMap(i -> postRepository.findActivePostsByPostOwner(i).stream())
-                        .map(DtoReturnPost::new)
-                        .toList();
+        userService.validateUserExistsById(userId);
+        var user = userService.getReferenceById(userId);
+        userService.validateUserActive(user);
 
-                return ResponseEntity.status(HttpStatus.OK).body(posts);
-            }
-            return ResponseEntity.status(HttpStatus.CONFLICT).body("ERROR: User is disabled.");
-        }
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("ERROR: User not found.");
+        var posts = user.getFollowing().stream()
+                .flatMap(i -> postRepository.findActivePostsByPostOwner(i).stream())
+                .map(DtoReturnPost::new)
+                .toList();
+
+        return ResponseEntity.status(HttpStatus.OK).body(posts);
     }
 
     @Transactional(readOnly = true)
     public ResponseEntity<?> getPostById(Long id) {
-        if (this.existsById(id)){
-            var post = this.getReferenceById(id);
+        validatePostExistsById(id);
 
-            if (!post.getActive()){
-                return ResponseEntity.status(HttpStatus.CONFLICT).body("ERROR: Post was deleted.");
-            }
+        var post = this.getReferenceById(id);
 
-            return ResponseEntity.status(HttpStatus.OK).body(new DtoReturnPost(post));
-        }
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("ERROR: Post not found.");
+        validatePostActive(post);
+
+        return ResponseEntity.status(HttpStatus.OK).body(new DtoReturnPost(post));
     }
 
     @Transactional
     public ResponseEntity<?> likePost(DtoLike data) {
-        if (!userService.existsById(data.userId()) || !this.existsById(data.postId())){
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("ERROR: User or Post not found.");
-        }
+        userService.validateUserExistsById(data.userId());
+        validatePostExistsById(data.postId());
 
         var post = this.getReferenceById(data.postId());
 
-        if (!post.getActive()){
-            return ResponseEntity.status(HttpStatus.CONFLICT).body("ERROR: Post was deleted.");
-        }
+        validatePostActive(post);
 
         var user = userService.getReferenceById(data.userId());
 
-        for (var i: post.getLikesByUserId()){
-            if (i.equals(user.getId())){
-                return ResponseEntity.status(HttpStatus.CONFLICT).body("ERROR: User already liked this post");
+        for (var i : post.getLikesByUserId()) {
+            if (i.equals(user.getId())) {
+                throw new LikedPostsException("User already liked this post");
             }
         }
 
         post.getLikesByUserId().add(data.userId());
-        post.setLikeCount(post.getLikeCount()+1);
+        post.setLikeCount(post.getLikeCount() + 1);
         this.save(post);
 
         return ResponseEntity.status(HttpStatus.OK).body(user.getUserName() + " LIKED THE POST");
@@ -179,28 +164,25 @@ public class PostService {
 
     @Transactional
     public ResponseEntity<?> dislikePost(DtoLike data) {
-        if (!userService.existsById(data.userId()) || !this.existsById(data.postId())){
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("ERROR: User or Post not found.");
-        }
+        userService.validateUserExistsById(data.userId());
+        validatePostExistsById(data.postId());
 
         var post = this.getReferenceById(data.postId());
 
-        if (!post.getActive()){
-            return ResponseEntity.status(HttpStatus.CONFLICT).body("ERROR: Post was deleted.");
-        }
+        validatePostActive(post);
 
         var user = userService.getReferenceById(data.userId());
 
-        for (var i: post.getLikesByUserId()){
-            if (i.equals(user.getId())){
+        for (var i : post.getLikesByUserId()) {
+            if (i.equals(user.getId())) {
                 post.getLikesByUserId().remove(i);
-                post.setLikeCount(post.getLikeCount()-1);
+                post.setLikeCount(post.getLikeCount() - 1);
                 this.save(post);
-                
+
                 return ResponseEntity.status(HttpStatus.OK).body("LIKE REMOVED");
             }
         }
-        return ResponseEntity.status(HttpStatus.CONFLICT).body("ERROR: User didn't like this post.");
+        throw new LikedPostsException("User didn't like this post.");
 
     }
 
@@ -211,6 +193,24 @@ public class PostService {
                 .toList();
 
         return ResponseEntity.status(HttpStatus.OK).body(publicPosts);
+    }
+
+    public void validatePostExistsById(Long id) {
+        if (!postRepository.existsById(id)) {
+            throw new PostNotFoundException(id);
+        }
+    }
+
+    private void validatePostOwner(Post post, User user, String message){
+        if (post.getPostOwner() != user){
+            throw new UnauthorizedUserException(message);
+        }
+    }
+
+    private void validatePostActive(Post post) {
+        if (!post.getActive()) {
+            throw new PostDeletedException();
+        }
     }
 
     public boolean existsById(Long id) {
