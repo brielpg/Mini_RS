ff17cfaccd782cd9f84dc20759c557ed9ad5d809
Gabriel Galiza <briel.galiza@gmail.com>
2026-07-19 23:10:25 -0300

    remove: comment entity, controller, service, repository, dtos

--- src/main/java/br/com/minirs/infrastructure/repositories/CommentRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.minirs.infrastructure.repositories;
-
-import br.com.minirs.domain.entities.Comments;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CommentRepository extends JpaRepository<Comments, Long>{
-}
